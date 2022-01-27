ad401b6dfb965f2c06422fba6b1c8c9104e3cde1
syedsxjjad <syedsxjjad@gmail.com>
2022-01-27 20:11:51 +0500

    arryfunction

---
+++ arrayfunction/index.js
@@ -0,0 +1,95 @@
+var arr=[12,43,45,34,87,76,55];
+
+
+//////_____Array Object_____/////
+var arr1=[
+    {fname:"Syed", Lname: "Sajjad"},
+    {fname:"Syed", Lname: "Sajid"},
+    {fname:"Syed", Lname: "Rehan"}
+]
+
+//////_____Filter_____//////
+
+// document.write(arr +"<br><br>");
+
+// var ess=arr.filter(fil);
+// document.write(ess +"<br><br>");
+
+
+// function fil(arr)
+// {
+//     return arr >=45;
+// }
+
+
+
+//////_____Map_____//////
+
+// let s=arr.map(mp);
+// document.write(s);
+// function mp(x)
+// {
+//    return x*5;
+// }
+
+
+// let s=arr1.map(mp);
+// document.write(s);
+// function mp(x)
+// {
+//   return x.fname + " " + x.Lname ;
+// }
+
+
+
+
+//////_____Reduce_____//////
+
+// let s=arr.reduce(red)
+// document.write(s);
+// function red(x,a) {
+//     return x +a
+// }
+
+
+
+
+//////_____Reduce & Reverse_____//////
+
+// //let s=arr.sort();
+// let a=arr.reverse();
+// //document.write(s,"<br>");
+// document.write(a);
+
+
+
+
+//////_____Some_____////// just one value check give me one value true or false
+
+// var b=arr.some(chk);
+// document.write(b);
+// function chk(ag) {
+//     return ag >=45;
+// }
+
+
+
+//////_____Every_____////// every value check then true or flse
+//all value check 
+
+// var b=arr.every(chk);
+// document.write(b);
+// function chk(ag) {
+//     return ag >=45;
+// }
+
+
+
+//////_____Find___Findindex____//////first value match 
+
+
+// var b=arr.find(chk);
+// document.write(b);
+// function chk(ag) {
+//     return ag >=45;
+// }
